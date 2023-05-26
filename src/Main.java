56152694ce8f7108a0ee2a6fa2725b712e86b94d
husniabed99@gmail.com <husniabed99@gmail.com>
2023-05-26 15:47:19 +0300

    initial commit feature branch

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,7 @@ public class Main {
 		public static void printSomething() {
 				System.out.println("Print something.");
 		}
+		public static void doSomething() {
+				System.out.println("Do Something.");
+		}
 }
