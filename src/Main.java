4683e6ee9cfb823901cf3020714f2715fd67df76
husniabed99@gmail.com <husniabed99@gmail.com>
2023-05-26 15:51:29 +0300

    modified main and printSomething methods

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 		public static void main(String[] args) {
-				System.out.println("Hello world!");
+				System.out.println("Hello world!!!");
 		}
 		public static void printSomething() {
-				System.out.println("Print something.");
+				System.out.println("Please print something.");
 		}
 		public static void doSomething() {
 				System.out.println("Do Something.");
