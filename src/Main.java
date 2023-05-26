845a59c99b0af2da5766193f09e84f102c124b72
husniabed99@gmail.com <husniabed99@gmail.com>
2023-05-26 15:53:27 +0300

    changes from featurebranch

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 		public static void main(String[] args) {
-				System.out.println("Hello world!");
+				System.out.println("Welcome world!");
 		}
 		public static void printSomething() {
 				System.out.println("Print something.");
 		}
 		public static void doSomething() {
-				System.out.println("Do Something.");
+				System.out.println("Please do Something.");
 		}
 }
